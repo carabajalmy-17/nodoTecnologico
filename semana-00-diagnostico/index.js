fa1df0e7af023da917bf2eea742383cfc61c9221
malena carabajal <carabajalmalena65@gmail.com>
2026-08-23 18:47:43 -0300

    modificacion  de la constante estudiante

--- semana-00-diagnostico/index.js
+++ semana-00-diagnostico/index.js
@@ -1,4 +1,4 @@
-const estudiante = "Malena Carabajal";
+const estudiante = "Malena Carabajal yuliana";
 
 console.log(`Entorno preparado para ${estudiante}`);
 console.log("¡Comienza el curso de Node.js!");
