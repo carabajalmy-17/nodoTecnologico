6de425fc2275f10e57a57b98e4d14557061ec473
malena carabajal <carabajalmalena65@gmail.com>
2026-08-23 22:15:59 -0300

    modificaciones

--- semana-01/tp-01-fundamentos-node/hola.js
+++
@@ -1,4 +0,0 @@
-console.log("Hola desde Node.js");
-
-console.log("Versión de Node.js:", process.version);
-console.log("Plataforma:", process.platform);
--- semana-01/tp-01-fundamentos-node/saludo.js
+++
@@ -1,2 +0,0 @@
-const nombre = process.argv[2] ?? "Malena Yuliana Carabajal";
-console.log(`Bienvenido, ${nombre}`);
